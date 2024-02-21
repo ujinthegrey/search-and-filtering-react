5b75d64c9518d1efbad15b4f80623afad1cfcff4
ujinthegrey <v4deus@gmail.com>
2024-02-21 16:18:23 +0400

    refactoring Sidebar

--- src/Sidebar/Colors/Colors.js
+++ src/Sidebar/Colors/Colors.js
@@ -2,10 +2,48 @@ import Input from '../../components/Input'
 
 import './Colors.css'
 
-function Colors() {
+function Colors({handleChange}) {
   return<div>
     <h2 className='sidebar-title color-title'>Colors</h2>
-    <Input/>
+    <label className='sidebar-label-container'>
+          <input onChange={handleChange} type="radio" value="" name="test3"/>
+          <span className='checkmark all'></span>All
+    </label>
+    <Input
+      handleChange={handleChange}
+      value="black"
+      color="black"
+      title="Black"
+      name="test3"
+    />
+    <Input
+      handleChange={handleChange}
+      value="white"
+      color="white"
+      title="Whitw"
+      name="test3"
+    />
+    <Input
+      handleChange={handleChange}
+      value="red"
+      color="red"
+      title="Red"
+      name="test3"
+    />
+    <Input
+      handleChange={handleChange}
+      value="green"
+      color="green"
+      title="Green"
+      name="test3"
+    />
+    <Input
+      handleChange={handleChange}
+      value="blue"
+      color="blue"
+      title="Blue"
+      name="test3"
+    />
   </div>
 }
 
